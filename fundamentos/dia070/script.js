61d538a024f7c84539bd76868008f1a5660993e5
Igor Santos <igorhnss@gmail.com>
2026-09-06 18:15:49 -0300

    operadores de atribuicao

---
+++ fundamentos/dia070/script.js
@@ -0,0 +1,13 @@
+let numero = 10;
+
+numero += 5; //adciona e atribui
+console.log(numero);
+
+numero -= 3; //subtrai e atribui
+console.log(numero);
+
+numero *= 5; //multiplica e atribui
+console.log(numero);
+
+numero /= 5; //divide e atribui
+console.log(numero);
