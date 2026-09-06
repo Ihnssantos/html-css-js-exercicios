9a1c930aadb37fe14140b3838218a198e6d226be
Igor Santos <igorhnss@gmail.com>
2026-09-06 18:25:01 -0300

    operadores de comparacao

---
+++ fundamentos/dia071/scripts.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 5;
+
+console.log(a > b); //maior
+console.log(a < b); //menor
+console.log(a >= b); //maior ou igual
+console.log(a <= b); //menor ou igual
+console.log(a == b); //igual em valor
+console.log(a === b); //igual em valor e tipo de dado
+console.log(a != b); //diferente em valor
+console.log(a !== b); //diferente em valor ou tipo de dado
