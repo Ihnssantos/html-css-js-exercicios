933ad3c805adc0f2a271a146b6533c854bc67597
Igor Santos <igorhnss@gmail.com>
2026-09-06 17:49:05 -0300

    const e reatribuicao

---
+++ fundamentos/dia067/script.js
@@ -0,0 +1,7 @@
+let variavel = "Pode mudar valor";
+const constante = "Não pode mudar o valor";
+
+variavel = "Valor reatribuido";
+//constante = "Erro ao tentar mudar";
+
+console.log(variavel); //novo valor reatribuido
