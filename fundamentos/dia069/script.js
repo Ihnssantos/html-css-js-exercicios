b6d7d9173d1f90761c0c36b58f820d9b96c1dd37
Igor Santos <igorhnss@gmail.com>
2026-09-06 18:12:37 -0300

    operadores aritmeticos

---
+++ fundamentos/dia069/script.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b); //soma
+console.log(a - b); //subtração
+console.log(a * b); //multiplicação
+console.log(a / b); //divisão
+console.log(a % b); //resto da divisão
