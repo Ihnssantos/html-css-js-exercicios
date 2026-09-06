30de70f7976a621204994b31261e92cc10c68023
Igor Santos <igorhnss@gmail.com>
2026-09-06 20:15:09 -0300

    condicional switch case

---
+++ fundamentos/dia074/script.js
@@ -0,0 +1,18 @@
+let opcao = 2;
+
+switch (opcao) {
+    case 1:
+        console.log("Cadastrar");
+        break;
+
+    case 2:
+        console.log("Consultar");
+        break;
+
+    case 3:
+        console.log("Excluir");
+        break;
+
+    default:
+        console.log("Opção inválida");
+}
