cd6c66d3e6b403bbfcac22581ea68215c187a517
Igor Santos <igorhnss@gmail.com>
2026-09-06 18:28:33 -0300

    operadores logicos

---
+++ fundamentos/dia072/script.js
@@ -0,0 +1,6 @@
+let idade = 25;
+let temCarteira = true;
+
+console.log(idade >= 18 && temCarteira); //maior de 18 E tem carteira?
+console.log(idade >= 18 || temCarteira); //maior de 18 OU tem carteira?
+console.log(!temCarteira); //não tem carteira?
