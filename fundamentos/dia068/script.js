204dd75e18bb1c155a2478311adab4a0543c61a6
Igor Santos <igorhnss@gmail.com>
2026-09-06 17:52:53 -0300

    tipos de dados

---
+++ fundamentos/dia068/script.js
@@ -0,0 +1,9 @@
+let nome = "Igor"; //string
+let idade = 25; //number
+let altura = 1.78; //number
+let programador = true; //boolean
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+console.log(programador);
