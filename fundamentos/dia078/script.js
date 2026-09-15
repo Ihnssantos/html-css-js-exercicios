cd291f16a309611d85f600642b3cc50a99ea0417
Igor Santos <igorhnss@gmail.com>
2026-09-14 22:00:05 -0300

    percorrendo arrays com for of e forEach

---
+++ fundamentos/dia078/script.js
@@ -0,0 +1,13 @@
+let linguagens = ["HTML", "CSS", "JavaScript"];
+
+//for of - linguagem recebe cada elemento do array
+for (let linguagem of linguagens) {
+    console.log(linguagem);
+}
+
+
+//forEach() - executa uma função para cada elemento do array
+linguagens.forEach(function(linguagem) {
+    console.log(linguagem);
+})
+
