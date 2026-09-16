db576cac7167b84425e82cf1e0ee6dc1eb88fa29
Igor Santos <igorhnss@gmail.com>
2026-09-15 21:12:27 -0300

    Alterando conteudo com JavaScript

---
+++ fundamentos/dia084/script.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector("#titulo");
+let texto = document.querySelector(".texto");
+
+titulo.textContent = "Titulo alterado pelo JS";
+texto.textContent = "Texto alterado pelo JS";
